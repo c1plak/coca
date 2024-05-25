b2354a0fa8ff690a541450172f122aa4f55bb89b
c1plak <fedill3783@gmail.com>
2024-05-25 18:56:07 +0700

    Created isights, testimonials, footer, pricing page.

---
+++ src/js/pricing.js
@@ -0,0 +1,6 @@
+import '../scss/pricing.scss';
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+
+useTheme();
+useBurger();
--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './src/index.html',
+        pricing: './src/pricing.html',
       },
     },
   },
